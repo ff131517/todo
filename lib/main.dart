d10232f12af0d814944b312cbafe301002b9864e
michaelFang <fangtongjs@gmail.com>
2020-08-10 19:44:52 +0800

    feat:test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget TitleSection = new Container(
+    Widget titleSection = new Container(
       padding: const EdgeInsets.all(32.0),
       child: new Row(
         children: <Widget>[
@@ -64,9 +64,41 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    // body: ;
+
     return new MaterialApp(
-        title: "Layout Demo",
-        theme: new ThemeData(primaryColor: Colors.blue //设置主题颜色
-            ));
+      title: "Layout Demo",
+      theme: new ThemeData(primaryColor: Colors.blue //设置主题颜色
+          ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new ListView(
+          children: [
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
   }
 }
+void main() => runApp(new MyApp());
