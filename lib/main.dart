34e71db500e9f78dc23a951ce05933b73ccc7e52
michaelFang <fangtongjs@gmail.com>
2020-08-12 18:30:40 +0800

    feat:fix img path

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class MyApp extends StatelessWidget {
       padding: const EdgeInsets.all(32.0),
       child: new Text(
         '''
-Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        sss
         ''',
         softWrap: true,
       ),
@@ -87,7 +87,7 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
         body: new ListView(
           children: [
             new Image.asset(
-              'images/lake.jpg',
+              'assets/images/lake.jpg',
               width: 600.0,
               height: 240.0,
               fit: BoxFit.cover,
